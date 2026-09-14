380fc7920c9f6bdf387442aec28c861f9457acad
Mahdi ebrahimi <Mahdieb1383@gmail.com>
2026-09-14 14:30:28 +0330

    finishing report and handler and security

--- Services/CostService.cs
+++ Services/CostService.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
 using NationalCodeCostProject.Data;
 using NationalCodeCostProject.DTOs;
@@ -16,7 +17,10 @@ public class CostService : ICostService
 
     public async Task<PagedResult<Cost>> GetCostsAsync(PaginationDto pagination, CostFilterDto filter)
     {
-        var query = _context.Costs.AsQueryable();
+        var query = _context.Costs.AsQueryable(); // یه کوئری برای فیلتر کردنه چون یه سری پارامتر اختیاری داریم بخوایم
+                                                  //برای هرکدوم لینک بنویسیم خیلی طولانی میشه بنابرای ن میگیم
+                                                  //یه کوئری بساز اگه فیلتر کدملی داشت فلان کار اگر فیلتر فلان داشت فلان کار
+                                                  //اخرش هم باید حتما تولیست  بزنیم  در انتها اگرنه  نمیره به دیتابیس
 
         if (!string.IsNullOrWhiteSpace(filter.NationalCode))
             query = query.Where(c => c.NationalCode.Contains(filter.NationalCode));
@@ -43,4 +47,28 @@ public class CostService : ICostService
             PageSize = pagination.PageSize
         };
     }
+    
+    
+       public async Task<CostReportViewModel> GetReportAsync()
+{
+    var hasAnyRecord = await _context.Costs.AnyAsync();
+
+    if (!hasAnyRecord)
+    {
+        return new CostReportViewModel();
+    }
+
+var result=_context.Cost.GroupBy(c=>1).Select(a=>new CostReportViewModel()
+{
+    TotalCount = g.Count(),
+            TotalAmount = g.Sum(c => c.Amount),
+            AverageAmount = g.Average(c => c.Amount),
+            MaxAmount = g.Max(c => c.Amount),
+            MinAmount = g.Min(c => c.Amount)
+});
+result.FirstAsync();
+return result;
+    
+}
+    
 }
